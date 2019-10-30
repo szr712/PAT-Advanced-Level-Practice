086099eecaaf34d5963774481662add3f1263ba2
szr712 <zirui990712@163.com>
2019-10-30 11:58:18 +0800

    19.10.30（2）

---
+++ 1053.cpp
@@ -0,0 +1,67 @@
+//对path的排序与网上代码有点不同，是直接控制dfs的顺序来保证输出的顺序
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<string>
+#include<vector>
+#include<map>
+#include<set>
+#include<cmath>
+#include<stack>
+using namespace std;
+
+struct node
+{
+	int weight;
+	int id;
+};
+
+int n, m;
+long long s;
+map<int, vector<node>> tree;
+vector<int> weight;
+int start;
+
+bool cmp(node a, node b) {
+	return a.weight > b.weight;
+}
+
+void find(vector<int> path,long long sum,int root) {
+	sum = sum + weight[root];
+	path.push_back(weight[root]);
+	if (tree.find(root) == tree.end()&& sum==s) {
+		printf("%d", path[0]);
+		for (int i = 1; i < path.size(); i++) {
+			printf(" %d", path[i]);
+		}
+		printf("\n");
+		return;
+	}
+	for (int i = 0; i < tree[root].size(); i++) {
+		find(path, sum, tree[root][i].id);
+	}
+	return;
+}
+
+int main() {
+	scanf("%d %d %lld", &n, &m, &s);
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		scanf("%d", &tmp);
+		weight.push_back(tmp);
+	}
+	for (int i = 0; i < m; i++) {
+		int id, child;
+		scanf("%d %d", &id, &child);
+		for (int j = 0; j < child; j++) {
+			node tmp;
+			scanf("%d", &tmp.id);
+			tmp.weight = weight[tmp.id];
+			tree[id].push_back(tmp);
+		}
+		sort(tree[id].begin(), tree[id].end(), cmp);
+	}
+	vector<int> path;
+	find(path, 0, 00);
+	system("PAUSE");
+}
